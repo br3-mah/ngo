a129539dfa63858487815f54de3ad04974889ac9
br3-mah <nyeleti.bremah@gmail.com>
2021-10-12 17:37:50 +0200

    ngo system - code offline

---
+++ database/factories/IntervensionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Intervension;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class IntervensionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Intervension::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
